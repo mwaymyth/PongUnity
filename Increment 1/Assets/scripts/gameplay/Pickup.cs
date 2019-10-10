89385ee15a9a7fdb1f73d2c3957ebb9e4b5017c3
Matthew Waymouth <matthewwaymouth@Matthews-MacBook-Pro.local>
2019-10-10 13:26:18 -0700

    Added all the increment 1 files for Wacky Pong

---
+++ Increment 1/Assets/scripts/gameplay/Pickup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : Ball
+{
+    int freezeDuration;
+    Timer freezeTimer;
+    ScreenSide side;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+        if (ballType == BallType.Freeze)
+        {
+            freezeDuration = ConfigurationUtils.FreezerEffectDuration;
+        }
+    }
+
+    /// <summary>
+    /// Detects collision with a ball to aim the ball
+    /// </summary>
+    /// <param name="coll">collision info</param>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        var ltPaddle = GameObject.FindGameObjectWithTag("LtPaddle").GetComponent<Paddle>();
+        var rtPaddle = GameObject.FindGameObjectWithTag("RtPaddle").GetComponent<Paddle>();
+
+        if (coll.gameObject.name == "RightPaddle")
+        {
+            ltPaddle.FreezePaddleDuration(freezeDuration);
+        }
+        else
+        {
+            rtPaddle.FreezePaddleDuration(freezeDuration);
+        }
+
+        // spawn a new ball and destroy self
+        Destroy(gameObject);
+        Camera.main.GetComponent<BallSpawner>().SpawnBall();
+
+    }
+}
---
+++ Increment 1/WackyPongDocumentation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WackyPongDocumentation
+{
+    /// <summary>
+    /// Dummy class to avoid recurring compiler problems
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Dummy method to avoid recurring compiler issues
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
